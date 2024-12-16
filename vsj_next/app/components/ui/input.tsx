f0333ec3bd243cddb439fc04922166316bb1bb78
Erwan Marega <maregaerwan@gmail.com>
2024-12-16 15:28:39 +0100

    Modification Inscription

--- vsj_next/app/components/ui/input.tsx
+++ vsj_next/app/components/ui/input.tsx
@@ -5,18 +5,23 @@ interface InputProps {
   type: string;
   name: string;
   placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input = ({ label, type, name, placeholder }: InputProps) => {
+const Input: React.FC<InputProps> = ({ label, type, name, placeholder, value, onChange }) => {
   return (
     <div className="flex flex-col">
-      {/* Label avec Mona Sans Semi-bold */}
-
-      {/* Texte des inputs avec Mona Sans Medium */}
+      <label htmlFor={name} className="text-sm font-medium">
+        {label}
+      </label>
       <input
+        id={name}
         type={type}
         name={name}
         placeholder={placeholder}
+        value={value}
+        onChange={onChange}
         className="border border-gray-300 rounded-md px-3 py-5 h-8 w-80 max-w-md text-black font-mona font-medium text-sm bg-transparent focus:outline-none focus:border-2 focus:border-black appearance-none placeholder:font-normal"
       />
     </div>
