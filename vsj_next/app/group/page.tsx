5efad52d3d012a2a25236bba61ef80d43a84c772
Ales230 <leszek.alan77@gmail.com>
2024-12-18 14:14:34 +0100

    maj

--- vsj_next/app/group/page.tsx
+++ vsj_next/app/group/page.tsx
@@ -42,28 +42,13 @@ export default function GroupePage() {
   }));
 
   return (
-    <div className="bg-gray-100 min-h-screen p-4 overflow-hidden select-none">
-      {/* Header */}
-      <div className="flex items-center justify-between mb-6 relative">
-        <div className="absolute left-1/2 transform -translate-x-1/2">
-          <Image src="/assets/img/logo.png" alt="Logo" width={65} height={40} />
-        </div>
-        <div className="flex items-center">
-          <Image
-            src="/assets/img/Group274.png"
-            alt="Avatar"
-            width={40}
-            height={40}
-          />
-        </div>
-      </div>
-
+    <div className="bg-gray-100 h-[92vh] p-4 overflow-hidden select-none grid grid-rows-2 grid-cols-2 gap-6">
       {/* Historique des performances */}
-      <div className="bg-white rounded-2xl shadow-md p-4 mb-6">
+      <div className="bg-white rounded-2xl shadow-md p-4 col-start-1 col-end-3 h-full w-full">
         <h2 className="text-xl font-semibold mb-3">
           Historique des performances
         </h2>
-        <div className="relative">
+        <div className="relative h-5/6">
           <div className="swiper-button-prev custom-swiper-button-prev"></div>
           <div className="swiper-button-next custom-swiper-button-next"></div>
           <Swiper
@@ -83,8 +68,8 @@ export default function GroupePage() {
             }}
           >
             {[...Array(8)].map((_, index) => (
-              <SwiperSlide key={index}>
-                <div className="bg-[#F5F5F5] rounded-2xl flex flex-col items-center p-4">
+              <SwiperSlide key={index} style={{ height: "100%" }}>
+                <div className="bg-[#F7F7F7] rounded-2xl flex flex-col items-center p-4 h-full justify-between">
                   <p className="text-lg font-medium mb-1">
                     Tournoi de Malreaux
                   </p>
@@ -121,125 +106,123 @@ export default function GroupePage() {
       </div>
 
       {/* Liste des nageurs */}
-      <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 mb-6">
-        <div className="bg-white rounded-3xl shadow-md p-4">
-          <h2 className="text-xl font-semibold mb-3 text-center bg-blue-500 text-white px-3 py-1 rounded-lg">
-            Liste des nageurs ({totalNageurs})
-          </h2>
-          <div className="bg-[#F5F5F5] p-4 rounded-3xl relative">
-            <ul
-              className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 transition-transform duration-300 ${
-                isAnimating
-                  ? "translate-y-4 opacity-50"
-                  : "translate-y-0 opacity-100"
+      <div className="bg-white rounded-3xl shadow-md p-4 h-full w-full">
+        <h2 className="text-lg font-semibold mb-3 text-left bg-blue-500 text-white px-3 py-1 rounded-lg">
+          Liste des nageurs ({totalNageurs})
+        </h2>
+        <div className="bg-[#F7F7F7] p-4 rounded-3xl relative">
+          <ul
+            className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 transition-transform duration-300 ${
+              isAnimating
+                ? "translate-y-4 opacity-50"
+                : "translate-y-0 opacity-100"
+            }`}
+          >
+            {nageurs
+              .slice(currentIndex, currentIndex + nageursParPage)
+              .map((nageur) => (
+                <li
+                  key={nageur.id}
+                  className="flex items-center justify-between border-b py-2"
+                >
+                  <div className="flex items-center space-x-3">
+                    <Image
+                      src="/assets/img/Group274.png"
+                      alt="Avatar"
+                      width={40}
+                      height={40}
+                    />
+                    <div>
+                      <p className="font-medium text-sm">{nageur.name}</p>
+                      <p className="text-xs text-gray-500">
+                        Nageur 100m | Om me nomme...
+                      </p>
+                    </div>
+                  </div>
+                  <p className="text-xs text-gray-600">{nageur.age} ans</p>
+                </li>
+              ))}
+          </ul>
+
+          <div className="flex justify-between mt-4">
+            <button
+              onClick={handleScrollUp}
+              disabled={currentIndex === 0}
+              className={`p-3 bg-blue-500 text-white rounded-full shadow-md hover:bg-blue-600 transition-transform duration-200 ease-in-out transform ${
+                currentIndex === 0
+                  ? "opacity-50 cursor-not-allowed"
+                  : "hover:scale-110"
               }`}
             >
-              {nageurs
-                .slice(currentIndex, currentIndex + nageursParPage)
-                .map((nageur) => (
-                  <li
-                    key={nageur.id}
-                    className="flex items-center justify-between border-b py-2"
-                  >
-                    <div className="flex items-center space-x-3">
-                      <Image
-                        src="/assets/img/Group274.png"
-                        alt="Avatar"
-                        width={40}
-                        height={40}
-                      />
-                      <div>
-                        <p className="font-medium text-sm">{nageur.name}</p>
-                        <p className="text-xs text-gray-500">
-                          Nageur 100m | Om me nomme...
-                        </p>
-                      </div>
-                    </div>
-                    <p className="text-xs text-gray-600">{nageur.age} ans</p>
-                  </li>
-                ))}
-            </ul>
-
-            <div className="flex justify-between mt-4">
-              <button
-                onClick={handleScrollUp}
-                disabled={currentIndex === 0}
-                className={`p-3 bg-blue-500 text-white rounded-full shadow-md hover:bg-blue-600 transition-transform duration-200 ease-in-out transform ${
-                  currentIndex === 0
-                    ? "opacity-50 cursor-not-allowed"
-                    : "hover:scale-110"
-                }`}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M5 15l7-7 7 7"
-                  />
-                </svg>
-              </button>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M5 15l7-7 7 7"
+                />
+              </svg>
+            </button>
 
-              <button
-                onClick={handleScrollDown}
-                disabled={currentIndex + nageursParPage >= totalNageurs}
-                className={`p-3 bg-blue-500 text-white rounded-full shadow-md hover:bg-blue-600 transition-transform duration-200 ease-in-out transform ${
-                  currentIndex + nageursParPage >= totalNageurs
-                    ? "opacity-50 cursor-not-allowed"
-                    : "hover:scale-110"
-                }`}
+            <button
+              onClick={handleScrollDown}
+              disabled={currentIndex + nageursParPage >= totalNageurs}
+              className={`p-3 bg-blue-500 text-white rounded-full shadow-md hover:bg-blue-600 transition-transform duration-200 ease-in-out transform ${
+                currentIndex + nageursParPage >= totalNageurs
+                  ? "opacity-50 cursor-not-allowed"
+                  : "hover:scale-110"
+              }`}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M19 9l-7 7-7-7"
-                  />
-                </svg>
-              </button>
-            </div>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M19 9l-7 7-7-7"
+                />
+              </svg>
+            </button>
           </div>
         </div>
+      </div>
 
-        {/* Prochaine compétition */}
-        <div className="bg-white rounded-2xl shadow-md p-4">
-          <h2 className="text-xl font-semibold mb-4 text-center">
-            Prochaine compétition
-          </h2>
-          <div className="bg-[#F5F5F5] rounded-2xl p-4 flex flex-col space-y-4">
-            <div className="flex flex-col space-y-1">
-              <p className="font-medium text-sm">Nom de compétition :</p>
-              <p className="text-xs text-gray-500">Nom de la compétition ici</p>
-            </div>
-            <div className="flex flex-col space-y-1">
-              <p className="font-medium text-sm">Adresse postale :</p>
-              <p className="text-xs text-gray-500">
-                Adresse de la compétition ici
-              </p>
-            </div>
-            <div className="flex flex-col space-y-1">
-              <p className="font-medium text-sm">Nages à effectuer :</p>
-              <p className="text-xs text-gray-500">
-                Liste des nages à effectuer ici
-              </p>
-            </div>
-            <div className="flex flex-col space-y-1">
-              <p className="font-medium text-sm">Équipements nécessaires :</p>
-              <p className="text-xs text-gray-500">Liste des équipements ici</p>
-            </div>
+      {/* Prochaine compétition */}
+      <div className="bg-white rounded-2xl shadow-md p-4 h-full w-full">
+        <h2 className="text-lg font-semibold mb-4 text-center">
+          Prochaine compétition
+        </h2>
+        <div className="bg-[#F7F7F7] rounded-2xl p-4 flex flex-col space-y-4">
+          <div className="flex flex-col space-y-1">
+            <p className="font-medium text-sm">Nom de compétition :</p>
+            <p className="text-xs text-gray-500">Nom de la compétition ici</p>
+          </div>
+          <div className="flex flex-col space-y-1">
+            <p className="font-medium text-sm">Adresse postale :</p>
+            <p className="text-xs text-gray-500">
+              Adresse de la compétition ici
+            </p>
+          </div>
+          <div className="flex flex-col space-y-1">
+            <p className="font-medium text-sm">Nages à effectuer :</p>
+            <p className="text-xs text-gray-500">
+              Liste des nages à effectuer ici
+            </p>
+          </div>
+          <div className="flex flex-col space-y-1">
+            <p className="font-medium text-sm">Équipements nécessaires :</p>
+            <p className="text-xs text-gray-500">Liste des équipements ici</p>
           </div>
         </div>
       </div>
