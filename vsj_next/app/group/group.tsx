a5dbd1dac189ef9cce3c5771865be8c61830e8f0
Ales230 <leszek.alan77@gmail.com>
2024-12-19 15:17:52 +0100

    maj

--- vsj_next/app/group/group.tsx
+++ vsj_next/app/group/group.tsx
@@ -19,7 +19,7 @@ export default function GroupePage() {
       setTimeout(() => {
         setCurrentIndex((prev) => Math.max(prev - nageursParPage, 0));
         setIsAnimating(false);
-      }, 300); // Durée de l'animation (300ms)
+      }, 300);
     }
   };
 
@@ -31,7 +31,7 @@ export default function GroupePage() {
           Math.min(prev + nageursParPage, totalNageurs - nageursParPage)
         );
         setIsAnimating(false);
-      }, 300); // Durée de l'animation (300ms)
+      }, 300);
     }
   };
 
@@ -45,8 +45,10 @@ export default function GroupePage() {
     <div className="bg-gray-100 dark:bg-gray-900 h-[92vh] p-4 grid grid-rows-2 grid-cols-2 gap-6">
       {/* Historique des performances */}
       <div className="bg-white dark:bg-gray-800 dark:text-white rounded-2xl shadow-md p-4 col-start-1 col-end-3 h-full w-full">
-        <h2 className="text-xl font-semibold mb-3 text-center">Historique des performances</h2>
-        <div className="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets swiper-pagination-horizontal" style={{ bottom: "15px" }}></div>
+        <h2 className="text-xl font-semibold mb-3 text-center">
+          Historique des performances
+        </h2>
+        <div className="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets swiper-pagination-horizontal"></div>
 
         <div className="relative h-5/6">
           <div className="swiper-button-prev custom-swiper-button-prev"></div>
@@ -60,7 +62,7 @@ export default function GroupePage() {
               nextEl: ".custom-swiper-button-next",
             }}
             pagination={{
-              el: ".swiper-pagination", // Pagination liée
+              el: ".swiper-pagination",
               clickable: true,
             }}
             breakpoints={{
@@ -72,35 +74,39 @@ export default function GroupePage() {
           >
             {[...Array(8)].map((_, index) => (
               <SwiperSlide key={index} style={{ height: "100%" }}>
-                <div className="bg-[#F7F7F7] dark:bg-gray-700 dark:text-white rounded-2xl flex flex-col items-center p-6 h-full justify-between transition-all hover:shadow-[0px_4px_10px_rgba(0,0,0,0.2)] dark:hover:shadow-[0px_4px_10px_rgba(255,255,255,0.2)]">
-                  <p className="text-lg font-medium mb-1 text-center">Tournoi de Malreaux</p>
-                  <p className="text-xs text-gray-600 dark:text-gray-400 mb-3">12 décembre 2023</p>
-                  <div className="text-base font-semibold mt-1 mb-3 text-center">4ème - 30"56'10</div>
-                  <p className="text-center text-xs text-gray-500 dark:text-gray-400 mb-4">
-                    Vous avez réalisé une performance d'équipe digne des plus grands, je vous félicite !
+                <div className="bg-[#F7F7F7] dark:bg-gray-700 dark:text-white rounded-2xl flex flex-col items-center p-4 h-full justify-between transition-all hover:shadow-[0px_4px_10px_rgba(0,0,0,0.2)] dark:hover:shadow-[0px_4px_10px_rgba(255,255,255,0.2)]">
+                  <p className="text-base font-medium mb-1 text-center">
+                    Tournoi de Malreaux
                   </p>
-                  <p className="text-center text-sm text-blue-600 dark:text-blue-400 mb-3 font-semibold">
-                    Meilleures performances
+                  <p className="text-xs text-gray-600 dark:text-gray-400 mb-2">
+                    12 déc. 2023
                   </p>
-                  <div className="flex justify-center space-x-7 mb-4">
+                  <p className="text-sm font-semibold mb-2 text-center">
+                    4ème - 30"56'10
+                  </p>
+                  <p className="text-center text-xs text-gray-500 dark:text-gray-400 mb-2">
+                    Une performance d'équipe remarquable !
+                  </p>
+
+                  <div className="flex justify-center space-x-5 mb-3">
                     {[...Array(3)].map((_, i) => (
-                      <div key={i} className="text-center">
+                      <div
+                        key={i}
+                        className="text-center flex flex-col items-center"
+                      >
                         <Image
                           src="/assets/img/Group274.png"
                           alt="Avatar"
                           width={40}
                           height={40}
-                          className="shadow-md" // Enlever ici la classe rounded-full
+                          className="shadow-md object-contain"
                         />
                         <p className="text-xs mt-1">Taylor</p>
-                        <p className="text-xs text-gray-500 dark:text-gray-400">30"56'10</p>
+                        <p className="text-xs text-gray-500 dark:text-gray-400">
+                          30"56'10
+                        </p>
                       </div>
                     ))}
-                  </div>
-                  <div className="text-center mt-2">
-                    <button className="bg-blue-600 text-white py-1 px-4 rounded-full text-xs hover:bg-blue-700 transition-colors">
-                      Voir plus
-                    </button>
                   </div>
                 </div>
               </SwiperSlide>
@@ -111,7 +117,7 @@ export default function GroupePage() {
 
       {/* Liste des nageurs */}
       <div className="bg-white dark:bg-gray-800 dark:text-white rounded-3xl shadow-md p-4 h-full w-full">
-        <h2 className="text-lg font-semibold mb-3 text-left bg-blue-500 dark:bg-blue-700 text-white px-3 py-2 rounded-lg inline-block">
+        <h2 className="text-lg font-semibold mb-2 text-left bg-blue-500 dark:bg-blue-700 text-white px-1 py-1 rounded-lg inline-block">
           Liste des nageurs ({totalNageurs})
         </h2>
         <div className="bg-[#F7F7F7] dark:bg-gray-700 p-4 rounded-3xl relative">
@@ -123,21 +129,28 @@ export default function GroupePage() {
             {nageurs
               .slice(currentIndex, currentIndex + nageursParPage)
               .map((nageur) => (
-                <li key={nageur.id} className="flex items-center justify-between border-b py-2">
+                <li
+                  key={nageur.id}
+                  className="flex items-center justify-between border-b py-2"
+                >
                   <div className="flex items-center space-x-3">
                     <Image
                       src="/assets/img/Group274.png"
                       alt="Avatar"
                       width={40}
                       height={40}
-                      className="shadow-md" // Enlever ici aussi la classe rounded-full
+                      className="shadow-md"
                     />
                     <div>
                       <p className="font-medium text-sm">{nageur.name}</p>
-                      <p className="text-xs text-gray-500 dark:text-gray-400">Nageur 100m | Om me nomme...</p>
+                      <p className="text-xs text-gray-500 dark:text-gray-400">
+                        Nageur 100m | Om me nomme...
+                      </p>
                     </div>
                   </div>
-                  <p className="text-xs text-gray-600 dark:text-gray-400">{nageur.age} ans</p>
+                  <p className="text-xs text-gray-600 dark:text-gray-400">
+                    {nageur.age} ans
+                  </p>
                 </li>
               ))}
           </ul>
@@ -198,23 +211,47 @@ export default function GroupePage() {
 
       {/* Prochaine compétition */}
       <div className="bg-white dark:bg-gray-800 dark:text-white rounded-2xl shadow-md p-4 h-full w-full">
-        <h2 className="text-lg font-semibold mb-4 text-left">Prochaine compétition</h2>
-        <div className="bg-[#F7F7F7] dark:bg-gray-700 rounded-2xl p-4 flex flex-col space-y-4">
-          <div className="flex flex-col space-y-1">
-            <p className="font-medium text-sm">Nom de compétition :</p>
-            <p className="text-xs text-gray-500 dark:text-gray-400">Nom de la compétition ici</p>
+        <h2 className="text-lg font-semibold mb-4 text-left">
+          Prochaine compétition
+        </h2>
+        <div className="bg-[#F7F7F7] dark:bg-gray-700 rounded-2xl p-4 flex space-x-6">
+          {/* flex et space-x-6 pour espacer l'image et les infos */}
+          {/* Informations de la compétition */}
+          <div className="flex-1">
+            <div className="flex flex-col space-y-1">
+              <p className="font-medium text-sm">Nom de compétition :</p>
+              <p className="text-xs text-gray-500 dark:text-gray-400">
+                Nom de la compétition ici
+              </p>
+            </div>
+            <div className="flex flex-col space-y-1">
+              <p className="font-medium text-sm">Adresse postale :</p>
+              <p className="text-xs text-gray-500 dark:text-gray-400">
+                Adresse de la compétition ici
+              </p>
+            </div>
+            <div className="flex flex-col space-y-1">
+              <p className="font-medium text-sm">Nages à effectuer :</p>
+              <p className="text-xs text-gray-500 dark:text-gray-400">
+                Liste des nages à effectuer ici
+              </p>
+            </div>
+            <div className="flex flex-col space-y-1">
+              <p className="font-medium text-sm">Équipements nécessaires :</p>
+              <p className="text-xs text-gray-500 dark:text-gray-400">
+                Liste des équipements ici
+              </p>
+            </div>
           </div>
-          <div className="flex flex-col space-y-1">
-            <p className="font-medium text-sm">Adresse postale :</p>
-            <p className="text-xs text-gray-500 dark:text-gray-400">Adresse de la compétition ici</p>
-          </div>
-          <div className="flex flex-col space-y-1">
-            <p className="font-medium text-sm">Nages à effectuer :</p>
-            <p className="text-xs text-gray-500 dark:text-gray-400">Liste des nages à effectuer ici</p>
-          </div>
-          <div className="flex flex-col space-y-1">
-            <p className="font-medium text-sm">Équipements nécessaires :</p>
-            <p className="text-xs text-gray-500 dark:text-gray-400">Liste des équipements ici</p>
+          {/* Image de la compétition */}
+          <div className="w-1/2">
+            <Image
+              src="/assets/img/image2.png"
+              alt="Prochaine compétition"
+              width={400}
+              height={150}
+              className="rounded-xl mx-auto"
+            />
           </div>
         </div>
       </div>
