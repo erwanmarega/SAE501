8d1282fb3e46e14eebc72411ec933211c6df85a4
antoine-censlas <antoine.moulin@edu.univ-eiffel.fr>
2025-01-04 17:06:11 +0100

    corrrection

--- vsj_next/app/map/page.tsx
+++ vsj_next/app/map/page.tsx
@@ -1,21 +1,23 @@
+"use client";
+
 import React, { useState } from "react";
 import Header from "../components/header/header";
 import BassinOlympique from "./bassin-olympique";
 
-const [selectedPage, setSelectedPage] = useState("bassin olympique");
-
-const whatShow = (value: string) => {
-  switch (value) {
-    case "bassin olympique":
-      return <BassinOlympique />;
-      break;
-
-    default:
-      break;
-  }
-};
-
 const MapPage = () => {
+  const [selectedPage, setSelectedPage] = useState("bassin olympique");
+
+  const whatShow = (value: string) => {
+    switch (value) {
+      case "bassin olympique":
+        return <BassinOlympique />;
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <div className="lg:h-[100vh] flex items-center justify-center lg:overflow-y-hidden bg-[#F7F7F7] dark:bg-[#262629]">
       {/* Composant d'en-tête */}
