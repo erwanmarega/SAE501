a28f5aaf3eff79d28e34ea1818cfc914ddb42285
Antoine Moulin <antoine.moulin@edu.univ-eiffel.fr>
2025-03-25 18:28:14 +0100

    save

---
+++ vsj_next/app/dashboard/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import "../globals.css";
+import React, { useState } from "react";
+import BottomBar from "../components/bottombar/bottom-bar";
+
+import Dashboard from "./dashboard";
+import Calendar from "../calendar/calendar";
+import Message from "../message/message";
+import Group from "../group/page";
+import Map from "../map/page";
+import StatsPage from "../stats/page";
+import clsx from "clsx";
+
+import Header from "../components/header/header";
+import { EventsProvider } from "../calendar/database/events-context";
+import Group02 from "../groupe02/group02";
+
+import MessageUpdate from "../message/message-update";
+
+export default function Home() {
+  const [currentPage, setCurrentPage] = useState("Dashboard");
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case "Dashboard":
+        return <Dashboard />;
+      case "Calendar":
+        return <Calendar />;
+      case "Message":
+        return <MessageUpdate />;
+      case "Group":
+        return <Group02 />;
+      case "Map":
+        return <Map />;
+      case "Stats":
+        return <StatsPage />;
+      default:
+        return <Dashboard />;
+    }
+  };
+
+  return (
+    <div
+      className={clsx(
+        "lg:h-[100vh] flex items-center justify-center lg:overflow-y-hidden",
+        { "lg:!h-[100vh]": currentPage === "Dashboard" }
+      )}
+    >
+      <Header />
+
+      <EventsProvider>{renderPage()}</EventsProvider>
+
+      <BottomBar
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+        isPage={currentPage !== "Dashboard"}
+      />
+    </div>
+  );
+}
--- vsj_next/app/page.tsx
+++ vsj_next/app/page.tsx
@@ -1,65 +1,19 @@
-"use client";
+import Hero from "./Landing/hero";
+import Features from "./Landing/features";
+import Stats from "./Landing/stats";
+import Testimonials from "./Landing/testimonials";
+import FAQ from "./Landing/faq";
+import LandingSection from "./Landing/section";
+import ActivitiesSection from "./Landing/communication";
 
-import "./globals.css";
-import React, { useState } from "react";
-import BottomBar from "./components/bottombar/bottom-bar";
-import Dashboard from "./dashboard/dashboard";
-import Calendar from "./calendar/calendar";
-import Message from "./message/message";
-import Group from "./group/page";
-import Map from "./map/page";
-import StatsPage from "./stats/page";
-import clsx from "clsx";
-import Header from "./components/header/header";
-import { EventsProvider } from "./calendar/database/events-context";
-import Group02 from "./groupe02/group02";
-import MessageUpdate from "./message/message-update";
-import Landing from "./Landing/page";
-
-export default function Home() {
-  const [currentPage, setCurrentPage] = useState("");
-
-  const renderPage = () => {
-    switch (currentPage) {
-      case "Dashboard":
-        return <Dashboard />;
-      case "Calendar":
-        return <Calendar />;
-      case "Message":
-        return <MessageUpdate />;
-      case "Group":
-        return <Group02 />;
-      case "Map":
-        return <Map />;
-      case "Stats":
-        return <StatsPage />;
-      default:
-        return <Landing />;
-    }
-  };
-
-  const isLandingPage = currentPage === "";
-
-  return isLandingPage ? (
-    <Landing />
-  ) : (
-    <div
-      className={clsx(
-        "lg:h-[100vh] flex items-center justify-center lg:overflow-y-hidden",
-        { "lg:!h-[100vh]": currentPage === "Dashboard" }
-      )}
-    >
-      <Header />
-
-      <EventsProvider>{renderPage()}</EventsProvider>
-
-      {!isLandingPage && (
-        <BottomBar
-          setCurrentPage={setCurrentPage}
-          currentPage={currentPage}
-          isPage={currentPage !== "Dashboard"}
-        />
-      )}
-    </div>
+export default function Landing() {
+  return (
+    <main className="bg-white text-gray-900">
+      <LandingSection />
+      <ActivitiesSection />
+      <Stats />
+      <Testimonials />
+      <FAQ />
+    </main>
   );
 }
